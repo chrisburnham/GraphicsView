dcf8bc5e8e680095d967182094d9074e3eef47af
chrisburnham <cburnham15@gmail.com>
2016-10-18 12:15:41 -0400

    added things that haven't worked

--- QGraphicsTest/my_pixmap.cpp
+++ QGraphicsTest/my_pixmap.cpp
@@ -7,4 +7,21 @@ My_pixmap::My_pixmap(QGraphicsItem *parent) :
   QPixmap pixmap(":/images/chart.png");
   setPixmap(pixmap);
   setTransformationMode(Qt::SmoothTransformation);
+
+  // Things I've tried that haven't helped
+  //setShapeMode(QGraphicsPixmapItem::BoundingRectShape);
+  //setPanelModality(QGraphicsItem::SceneModal);
 }
+
+void My_pixmap::paint(QPainter *painter,
+                      const QStyleOptionGraphicsItem *option,
+                      QWidget *widget)
+{
+  // Things I've tried that haven't helped
+  //painter->setRenderHint(QPainter::NonCosmeticDefaultPen, true);
+  //painter->setRenderHint(QPainter::Antialiasing, true);
+  //painter->setRenderHint(QPainter::HighQualityAntialiasing, true);
+
+  QGraphicsPixmapItem::paint(painter, option, widget);
+}
+
